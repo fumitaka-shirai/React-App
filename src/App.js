b39c290a9d7abba91f9bc466a0f079b9fe644ea5
fumitaka-shirai <130987855+fumitaka-shirai@users.noreply.github.com>
2023-04-27 10:58:07 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import{ Amplify }from "aws-amplify";
 import{ withAuthenticator } from "@aws-amplify/ui-react";
 import"@aws-amplify/ui-react/styles.css";
-
 import awsExports from "./aws-exports";
+
 Amplify.configure(awsExports);
 
 function App({signOut, user }) {
